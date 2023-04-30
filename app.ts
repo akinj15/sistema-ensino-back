aa94771866f65a86d2f0432b085cdaa1c97e5451
akinj15 <akin.fantuci@gmail.com>
2023-04-30 15:39:49 -0300

    att

--- app.ts
+++ app.ts
@@ -6,10 +6,6 @@ let app = express();
 
 const allowedOrigins = [
     '*', 
-    'http://localhost:9001', 
-    'http://localhost:8080', 
-    'http://127.0.0.1:9001', 
-    'http://127.0.0.1:8080', 
 ];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
