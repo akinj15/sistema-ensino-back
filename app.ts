433b964dfe5b4247b22588b8b1146a1622d5083c
akinj15 <akin.fantuci@gmail.com>
2023-04-30 18:40:00 -0300

    att

--- app.ts
+++ app.ts
@@ -2,15 +2,14 @@ import * as express from "express"
 import router from "./src/routes"
 const cors = require('cors')
 let app = express();
-let corsOptions = {
-    origin: "*"
-};
-
 
 // app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ credentials : true, origin : ["https://grand-travesseiro-2c3fa7.netlify.app/"]}));
+app.use(cors({ 
+  credentials : true, 
+  origin : "*"
+}));
 app.use(router);
 
 export default app;
